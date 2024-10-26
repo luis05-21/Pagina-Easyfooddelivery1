f2df5e871967b8a347203655f452ebde76e13c29
luis05-21 <162893309+luis05-21@users.noreply.github.com>
2024-10-26 17:04:53 -0500

    Add files via upload

---
+++ formulario.js
@@ -0,0 +1,4 @@
+document.getElementById("formularioparapedidos").addEventListener("submit", function(event) {
+    event.preventDefault(); // Previene el envío del formulario y recarga de la página
+    alert("Su Pedido fue Recibido");
+});
